7ee8f700dfc6e4c590a00893239302316236058a
asanebou.mackey <asanebou.job4219@gmail.com>
2022-06-28 16:07:24 +0900

    ストップウォッチ

---
+++ index.js
@@ -0,0 +1,9 @@
+const timer = document.querySelector(".timer");
+//console.log(timer);
+const startBtn = document.querySelector(".start");
+//console.log(startBtn);
+const stopBtn = document.querySelector(".stop");
+//console.log(stopBtn);
+const resetBtn = document.querySelector(".reset");
+//console.log(resetBtn);
+let elapsedTime = 0;
